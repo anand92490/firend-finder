d71d1f82b21119ab73b5dc82fcf27ae34ff34e35
Anand Chaudhary <anand92490@gmail.com>
2019-09-11 22:28:40 -0500

    friends data added

---
+++ app/data/friend.js
@@ -0,0 +1,64 @@
+var friends = [
+    {
+        name: "Shrinkhala Khatiwada",
+        photo: "https://www.sanjaal.com/content/wp-content/uploads/2018/11/srinkhala_khatiwada_miss_nepal_2018.jpg",
+        scores: [5, 2, 3, 5, 3, 2, 4, 2, 1, 4]
+    },
+
+    {
+        name: "Shelisha Pitt",
+        photo: "https://thenypost.files.wordpress.com/2019/04/nike-548.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+        scores: [4, 2, 3, 1, 5, 5, 3, 4, 5, 2]
+    },
+
+    {
+        name: "Catriona Gray",
+        photo: "https://media.philstar.com/photos/2019/01/09/catriona-gray-filipina-australian_2019-01-09_16-24-07.jpg",
+        scores: [3, 4, 2, 5, 1, 3, 3, 5, 2, 1]
+    },
+
+    {
+        name: "Tong Mei Thuong",
+        photo: "http://img3.blogtamsu.vn/2017/03/tung-son-1.jpg",
+        scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 3]
+    },
+
+    {
+        name: "Hollini McLovin",
+        photo: "https://imgflip.com/memetemplate/89150100/Ugly-Girl",
+        scores: [4, 2, 2, 1, 4, 3, 4, 1, 3, 4]
+    },
+
+    {
+        name: "Lolita Evergreen",
+        photo: "https://www.betrendsetter.com/wp-content/uploads/2017/04/best-haircolor-light-brown-skin-2.jpg",
+        scores: [2, 2, 3, 1, 3, 2, 5, 5, 1, 3]
+    },
+
+    {
+        name: "Samuel Adams",
+        photo: "https://data.whicdn.com/images/162198973/large.jpg",
+        scores: [4, 2, 1, 3, 5, 5, 2, 2, 2, 5]
+    },
+
+    {
+        name: "David Silva",
+        photo: "https://i1.wp.com/media.boingboing.net/wp-content/uploads/2018/09/screenshot-26-2.jpg?resize=970%2C541&ssl=1",
+        scores: [3, 4, 1, 5, 2, 5, 3, 4, 2, 4]
+    },
+
+    {
+        name: "linda Knapp",
+        photo: "https://i.pinimg.com/originals/bb/79/3a/bb793aa8213ecf62caa0679186922e2a.jpg",
+        scores: [4, 3, 2, 5, 1, 5, 4, 2, 3, 4]
+    },
+
+    {
+        name: "Rose O'lordno",
+        photo: "https://cdn.ebaumsworld.com/mediaFiles/picture/565775/81394960.jpg",
+        scores: [5, 2, 3, 1, 4, 2, 3, 2, 1, 4]
+    },
+
+];
+
+module.exports = friends;
